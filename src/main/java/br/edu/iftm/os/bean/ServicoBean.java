91057accca95b8eec25973bef22c1135e497ff3d
brunoCreator <brunoCreator@users.noreply.github.com>
2021-01-27 13:54:27 -0300

    Sincronização

--- src/main/java/br/edu/iftm/os/bean/ServicoBean.java
+++ src/main/java/br/edu/iftm/os/bean/ServicoBean.java
@@ -1,30 +1,24 @@
 package br.edu.iftm.os.bean;
 
 import br.edu.iftm.os.logic.ClienteLogic;
-import br.edu.iftm.os.logic.ServicoLogic;
 import br.edu.iftm.os.model.Cliente;
-import br.edu.iftm.os.model.Servico;
 import javax.enterprise.context.SessionScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
-import lombok.Getter;
-import lombok.Setter;
 
 
 @Named
 @SessionScoped
-@Getter @Setter
-public class ServicoBean extends CrudBean<Servico, ServicoLogic> {
+public class ServicoBean extends CrudBean<Cliente, ClienteLogic> {
 
-    @Inject
-    private ServicoLogic logic;
-    
     public ServicoBean() {
-        super(Servico.class);
+        super(Cliente.class);
     }
-        
+    
     @Override
-    public ServicoLogic getLogic() {
-        return this.logic;
+    public ClienteLogic getLogic() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    
+    
 }
--- src/main/java/br/edu/iftm/os/repository/StatusRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.iftm.os.repository;
-
-import br.edu.iftm.os.model.Status;
-
-
-public class StatusRepository extends GenericRepository<Status, Integer> {
-    
-}
